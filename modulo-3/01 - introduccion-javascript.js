e50aabcdddbb6fc1003e1facf3603bbfe58554c8
Adrián Solimano <adrian.solimano@ibisdev.tech>
2022-02-06 22:10:43 -0300

    Contenido clase 04-02-22

---
+++ modulo-3/01 - introduccion-javascript.js
@@ -0,0 +1,9 @@
+var color = "Amarillo";
+
+// var color = 452361; // funciona
+
+// color = 452361; // funciona
+
+// color = "rgb(93, 151, 25)"; // funciona
+
+// color = 452361 // funciona
---
+++ modulo-3/01 - introduccion-javascript.ts
@@ -0,0 +1,20 @@
+var color2: string = "Amarillo";
+
+// var color2: string = 452361; // no funciona
+
+// color2 = 452361; // no funciona
+
+// color2 = "rgb(93, 151, 25)"; // funciona
+
+// color2 = 452361; // no funciona
+
+/* Definición de tipo por inferencia */
+
+var nombre = "Adrián";
+
+var color4;
+
+color4 = "Amarillo";
+
+color4 = 123123;
+
